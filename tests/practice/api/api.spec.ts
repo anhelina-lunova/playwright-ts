f5b78c06bbfcb8ef80c88c5954071a9c43879707
Angie Lina <120516170+anhelina-lunova@users.noreply.github.com>
2026-06-09 17:18:09 +0300

    practice api

--- tests/practice/api/api.spec.ts
+++ tests/practice/api/api.spec.ts
@@ -1,1 +1,72 @@
 import { test, expect } from '@playwright/test';
+
+test('Get all posts', async ({ request }) => {
+    const response = await request.get(
+        'https://jsonplaceholder.typicode.com/posts',
+    );
+
+    // console.log(response);
+    // console.log(response.status());
+    // console.log(await response.json());
+    const posts = await response.json();
+    const firstPost = posts[0];
+    expect(response.status()).toBe(200);
+    expect(posts).toHaveLength(100);
+    expect(firstPost.title).toContain('facere repellat provident');
+});
+
+test('Create new post', async ({ request }) => {
+    const response = await request.post(
+        'https://jsonplaceholder.typicode.com/posts',
+        { data: { title: 'foo', body: 'bar', userId: 1 } },
+    );
+
+    const createdPost = await response.json();
+    expect(response.status()).toBe(201);
+    expect(createdPost.title).toBe('foo');
+    expect(createdPost.body).toBe('bar');
+    expect(createdPost.userId).toBe(1);
+});
+
+test('Create new post with object', async ({ request }) => {
+    const newPost = { title: 'foo', body: 'bar', userId: 1 };
+
+    const response = await request.post(
+        'https://jsonplaceholder.typicode.com/posts',
+        { data: newPost },
+    );
+
+    const createdPost = await response.json();
+    expect(response.status()).toBe(201);
+    expect(createdPost.title).toBe(newPost.title);
+    expect(createdPost.body).toBe(newPost.body);
+    expect(createdPost.userId).toBe(newPost.userId);
+});
+
+test('Update a post', async ({ request }) => {
+    const newPostData = { title: 'foo', body: 'bar', userId: 1 };
+
+    const response = await request.put(
+        'https://jsonplaceholder.typicode.com/posts/3',
+        { data: newPostData },
+    );
+
+    const updatedPost = await response.json();
+    console.log(updatedPost);
+
+    expect(response.status()).toBe(200);
+    expect(updatedPost.title).toBe(newPostData.title);
+    expect(updatedPost.body).toBe(newPostData.body);
+    expect(updatedPost.userId).toBe(newPostData.userId);
+});
+
+test('Delete a post', async ({ request }) => {
+    const response = await request.delete(
+        'https://jsonplaceholder.typicode.com/posts/5',
+    );
+
+    // console.log(await response.text());
+    expect(response.status()).toBe(200);
+});
+
+test.describe;
---
+++ tests/practice/api/qauto-api.spec.ts
@@ -0,0 +1,73 @@
+import { test, expect } from '@playwright/test';
+import { testUser1 } from '../../../test-data/testUsers';
+
+test.describe('Unauthenticated', async () => {
+    test('Get all brands', async ({ request }) => {
+        const response = await request.get('/api/cars/brands');
+        const responseJson = await response.json();
+        const brands = responseJson.data;
+        const firstBrand = brands[0];
+        expect(response.status()).toBe(200);
+        expect(brands).toHaveLength(5);
+        expect(firstBrand.title).toBe('Audi');
+        // console.log(await response.json());
+    });
+
+    test('Get all models', async ({ request }) => {
+        const response = await request.get('/api/cars/models');
+        const responseJson = await response.json();
+        const models = responseJson.data;
+        const firstModel = models[0];
+
+        // console.log(models);
+
+        expect(response.status()).toBe(200);
+        expect(models).toHaveLength(23);
+        expect(firstModel.title).toBe('TT');
+    });
+
+    test('Should return 401 on Add a car attempt as unauthenticated user', async ({
+        request,
+    }) => {
+        const newCarData = {
+            carBrandId: 1,
+            carModelId: 1,
+            mileage: 122,
+        };
+
+        const response = await request.post('/api/cars', {
+            data: newCarData,
+        });
+        expect(response.status()).toBe(401);
+    });
+
+    test('Sign in', async ({ request }) => {
+        const existingUserData = {
+            email: testUser1.email,
+            password: testUser1.password,
+            remember: false,
+        };
+        const response = await request.post('/api/auth/signin', {
+            data: existingUserData,
+        });
+        expect(response.status()).toBe(200);
+
+        const sid = response.headers()['set-cookie'].split(';')[0];
+        expect(sid).toContain('sid=');
+    });
+});
+
+test.describe('Authenticated', async () => {
+    test('Add a car', async ({ request }) => {
+        const newCarData = {
+            carBrandId: 1,
+            carModelId: 1,
+            mileage: 122,
+        };
+
+        const response = await request.post('/api/cars', {
+            data: newCarData,
+        });
+        expect(response.status()).toBe(401);
+    });
+});
